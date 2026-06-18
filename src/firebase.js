d68d06332284fac05495fd4e91f83f7502f221db
SUZY <victoria1230120@gmail.com>
2026-06-18 16:59:29 +0900

    use base64 encoded service account for Firebase

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,10 @@ const { initializeApp, getApps, cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
 
 if (!getApps().length) {
-  initializeApp({
-    credential: cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-    }),
-  });
+  const serviceAccount = JSON.parse(
+    Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT_BASE64, 'base64').toString('utf8')
+  );
+  initializeApp({ credential: cert(serviceAccount) });
 }
 
 const db = getFirestore();
